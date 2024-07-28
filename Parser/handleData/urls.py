83e637cb5652b07585785a7ed2f3fd1b274baa81
Sanskar Singh <work.sanskarr@gmail.com>
2024-07-28 12:48:55 +0530

    Created Input Fields and rendered frontend

--- Parser/handleData/urls.py
+++ Parser/handleData/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('parse/', views.parsing, name='parsing')
+    # path('parse/', views.parsing, name='parsing')
+    path('',views.homePage)
 ]
--- Parser/handleData/views.py
+++ Parser/handleData/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+
+def homePage(request):
+    return  render(request,"index.html")
+
 def parsing(request):
     return HttpResponse("hello world")
